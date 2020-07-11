2137483c9065c546f563af12f6becda33fe69910
Isaac de Souza <isaacde2012@gmail.com>
2020-07-10 23:35:33 -0300

    Conexão com o BD feito com sucesso. o projeto agora é apto de guardar informaçoesde maneira permanente num sistema bem modularizado.

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import Router from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    res.json({
-        hello: 'worldo',
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'isaac',
+        email: 'email@top.com',
+        password_hash: '123',
     });
+
+    res.json(user);
 });
 
 export default routes;
