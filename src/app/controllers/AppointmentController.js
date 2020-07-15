85807971adaeaaa80611d4bf2f2c7cfec826ea66
Isaac de Souza <isaacde2012@gmail.com>
2020-07-14 22:43:43 -0300

    Listagem dos agendamentos para o prestador de serviço feita. paginação ao retornar a listagem de agendamentos para o usuário implementada.

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -10,6 +10,7 @@ import File from '../models/File';
 
 class AppointmentController {
     async index(req, res) {
+        const { page = 1 } = req.query;
         const appointments = await Appointment.findAll({
             where: {
                 user_id: req.userId,
@@ -17,6 +18,8 @@ class AppointmentController {
             },
             attributes: ['id', 'date'],
             order: ['date'],
+            limit:20,
+            offset: (page - 1) * 20,
             include: [{
                 model: User,
                 as: 'provider',
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import auth from './app/middlewares/auth';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -16,9 +17,10 @@ routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(auth);
+routes.get('/schedule', ScheduleController.index);
 routes.get('/providers', ProviderController.index);
-routes.get('/appointments',AppointmentController.index);
+routes.get('/appointments', AppointmentController.index);
 routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
-routes.post('/appointments',AppointmentController.store);
+routes.post('/appointments', AppointmentController.store);
 export default routes;
