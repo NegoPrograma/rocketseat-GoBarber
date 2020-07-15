ae6bd99cca01c35b915053274501784dcbfec817
Isaac de Souza <isaacde2012@gmail.com>
2020-07-14 23:29:40 -0300

    Sistema de notificações implementado com banco NoSQL por questões de performance e falta de necessidade de uma estrutura fixa.

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,12 +1,21 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import {
+    startOfHour,
+    parseISO,
+    isBefore,
+    format
+} from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
     async index(req, res) {
-        const { page = 1 } = req.query;
+        const {
+            page = 1
+        } = req.query;
         const appointments = await Appointment.findAll({
             where: {
                 user_id: req.userId,
@@ -16,20 +25,16 @@ class AppointmentController {
             order: ['date'],
             limit: 20,
             offset: (page - 1) * 20,
-            include: [
-                {
-                    model: User,
-                    as: 'provider',
-                    attributes: ['id', 'name'],
-                    include: [
-                        {
-                            model: File,
-                            as: 'avatar',
-                            attributes: ['id', 'path', 'url'],
-                        },
-                    ],
-                },
-            ],
+            include: [{
+                model: User,
+                as: 'provider',
+                attributes: ['id', 'name'],
+                include: [{
+                    model: File,
+                    as: 'avatar',
+                    attributes: ['id', 'path', 'url'],
+                }, ],
+            }, ],
         });
         return res.json(appointments);
     }
@@ -46,7 +51,10 @@ class AppointmentController {
             });
         }
 
-        const { provider_id, date } = req.body;
+        const {
+            provider_id,
+            date
+        } = req.body;
 
         /**
          * Check if the provided id really belongs
@@ -98,6 +106,21 @@ class AppointmentController {
             user_id: req.userId,
             provider_id,
             date: hourStart,
+        });
+
+        /**
+         * Notify appointment.
+         */
+        const user = await User.findByPk(req.userId);
+        const formattedDate = format(
+            hourStart,
+            "'dia' dd 'de' MMMM', às' H:mm'h'", {
+                locale: pt
+            }
+        );
+        await Notification.create({
+            content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+            user: provider_id,
         });
         return res.json(appointment);
     }
